65d00dc2b755831549548d883bbbaf044f68370d
OpenShift guest <cstrauber@gmail.com>
2013-08-02 15:26:43 -0400

    Add new_faculty_names2.py

---
+++ NewFaculty/new_faculty_names2.py
@@ -0,0 +1,8 @@
+import csv
+f = open('new_faculty.csv', 'rb')
+listicle = csv.reader(f)
+departments = ['Classics', 'English', 'Philosophy', 'Religion']
+for row in listicle:
+	for department in departments:
+		if department in row:
+			print row[0] + ' ' + row[1] + ', ' + row[2]
